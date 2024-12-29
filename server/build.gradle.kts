354a408172b71ae3a604e67d717ea91806ee5d7c
ShinzenATT <alen@shinzle.net>
2024-12-29 12:38:42 +0100

    Setup spring boot in server

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.ktor)
+    alias(libs.plugins.springBoot)
+    alias(libs.plugins.springDependencyManagement)
+    alias(libs.plugins.springDoc)
+    alias(libs.plugins.asciidoctor)
+    alias(libs.plugins.kotlinxSerialization)
     application
 }
 
@@ -8,14 +12,43 @@ group = "se.chalmers.hd"
 version = "1.0.0"
 application {
     mainClass.set("se.chalmers.hd.ApplicationKt")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=${extra["io.ktor.development"] ?: "false"}")
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(23)
+    }
 }
 
 dependencies {
     implementation(projects.shared)
-    implementation(libs.logback)
-    implementation(libs.ktor.server.core)
-    implementation(libs.ktor.server.netty)
-    testImplementation(libs.ktor.server.tests)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.quartz)
+    implementation(libs.spring.boot.starter.webflux)
+    implementation(libs.spring.boot.starter.websocket)
+    implementation(libs.jackson.module.kotlin)
+    implementation(libs.reactor.kotlin.extensions)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.reactor)
+    implementation(libs.spring.session.core)
+    implementation(libs.kotlinx.serialization)
+
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.datetime)
+    implementation(libs.exposed.spring.boot)
+    implementation(libs.postgresql)
+
     testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.reactor.test)
+    testImplementation(libs.spring.restdocs.webtestclient)
+    testRuntimeOnly(libs.junit.platform.launcher)
+    testRuntimeOnly(libs.h2)
+
 }
