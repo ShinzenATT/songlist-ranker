95dbf723b0198df3d1c048d150fdf9cdbf4520e8
ShinzenATT <alen@shinzle.net>
2024-12-29 16:55:47 +0100

    Updated constants and about info

---
+++ shared/src/commonMain/kotlin/se/chalmers/hd/About.kt
@@ -0,0 +1,11 @@
+package se.chalmers.hd
+
+class About {
+    val title = "HD-sektionens sångbok"
+    val published = "2025"
+    val developer = listOf("Shinz HD21/22", "Meta HD20/21")
+    val original_title = "H-Sektionens sångbok"
+    val original_publish_date = "1999"
+    val original_intiativ = "H-Sex 99"
+
+}
--- shared/src/commonMain/kotlin/se/chalmers/hd/Constants.kt
+++ shared/src/commonMain/kotlin/se/chalmers/hd/Constants.kt
@@ -1,3 +1,4 @@
 package se.chalmers.hd
 
+const val SERVER_URL = "http://localhost"
 const val SERVER_PORT = 8080
