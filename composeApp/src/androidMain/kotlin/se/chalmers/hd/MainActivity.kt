db3f4eea8999eb37418ebe6c8cd2170b8df34d12
ShinzenATT <alen@shinzle.net>
2025-04-06 17:23:54 +0200

    Added colorScheme and added dynamic color support

--- composeApp/src/androidMain/kotlin/se/chalmers/hd/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/se/chalmers/hd/MainActivity.kt
@@ -1,8 +1,13 @@
 package se.chalmers.hd
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -11,7 +16,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            var colorScheme: ColorScheme? = null
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                colorScheme = if (isSystemInDarkTheme()){
+                    dynamicDarkColorScheme(applicationContext)
+                } else {
+                    dynamicLightColorScheme(applicationContext)
+                }
+            }
+
+            App(colorScheme)
         }
     }
 }
--- composeApp/src/commonMain/kotlin/se/chalmers/hd/App.kt
+++ composeApp/src/commonMain/kotlin/se/chalmers/hd/App.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import se.chalmers.hd.theme.AppTheme
 import se.chalmers.hd.views.HomeView
 import se.chalmers.hd.views.SongListView
 import se.chalmers.hd.views.asScreenView
@@ -16,51 +17,60 @@ import se.chalmers.hd.views.asScreenView
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
-fun App() {
-    Navigator(HomeView()) { navigator ->
-        val currentPage = navigator.lastItem.asScreenView()
-        MaterialTheme {
-            Scaffold(
-                topBar = {
-                    if(currentPage.topBarVisible) {
-                        TopAppBar(
-                            title = { currentPage.topBarTitle() },
-                            navigationIcon = {
-                                if (navigator.lastItem is HomeView) return@TopAppBar
-                                IconButton(onClick = { navigator.pop() }) {
-                                    Icon(
-                                        Icons.AutoMirrored.Filled.ArrowBack,
-                                        contentDescription = "Back"
-                                    )
+fun App(colorScheme: ColorScheme? = null) {
+    AppTheme(dynamicColorTheme = colorScheme) {
+        Navigator(HomeView()) { navigator ->
+            val currentPage = navigator.lastItem.asScreenView()
+            MaterialTheme {
+                Scaffold(
+                    topBar = {
+                        if (currentPage.topBarVisible) {
+                            TopAppBar(
+                                title = { currentPage.topBarTitle() },
+                                navigationIcon = {
+                                    if (navigator.lastItem is HomeView) return@TopAppBar
+                                    IconButton(onClick = { navigator.pop() }) {
+                                        Icon(
+                                            Icons.AutoMirrored.Filled.ArrowBack,
+                                            contentDescription = "Back"
+                                        )
+                                    }
                                 }
-                            }
-                        )
+                            )
+                        }
+                    },
+                    content = {
+                        currentPage.rootPadding = it
+                        CurrentScreen()
+                    },
+                    bottomBar = {
+                        NavigationBar {
+                            NavigationBarItem(
+                                icon = { Icon(imageVector = Icons.Default.PlayArrow, contentDescription = null) },
+                                selected = navigator.lastItem is HomeView,
+                                label = { Text("Slå Up") },
+                                onClick = {
+                                    navigator.popUntilRoot()
+                                },
+                            )
+                            NavigationBarItem(
+                                icon = {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Filled.List,
+                                        contentDescription = null
+                                    )
+                                },
+                                selected = navigator.lastItem is SongListView,
+                                label = { Text("Bläddra") },
+                                onClick = {
+                                    navigator.popUntilRoot()
+                                    navigator.push(SongListView())
+                                },
+                            )
+                        }
                     }
-                 },
-                content = {
-                    currentPage.rootPadding = it
-                    CurrentScreen()
-                  },
-                bottomBar = { NavigationBar {
-                    NavigationBarItem(
-                        icon = { Icon(imageVector = Icons.Default.PlayArrow, contentDescription = null) },
-                        selected = navigator.lastItem is HomeView,
-                        label = { Text("Slå Up") },
-                        onClick = {
-                            navigator.popUntilRoot()
-                        },
-                    )
-                    NavigationBarItem(
-                        icon = { Icon(imageVector = Icons.AutoMirrored.Filled.List, contentDescription = null) },
-                        selected = navigator.lastItem is SongListView,
-                        label = { Text("Bläddra") },
-                        onClick = {
-                            navigator.popUntilRoot()
-                            navigator.push(SongListView())
-                        },
-                    )
-                } }
-            )
+                )
+            }
         }
     }
 }
